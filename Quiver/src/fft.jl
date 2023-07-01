92dd29addac44a121adb8e0ecb91fd31bb742f8d
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-07-01 19:29:01 +0800

    add: `FFTKernel`, `fft!`, `fft`

--- Quiver/src/fft.jl
+++ Quiver/src/fft.jl
@@ -1,6 +1,22 @@
 module FFT
 
-function twiddle!(wa::Vector{Complex{T}}) where T<:AbstractFloat
+const VecI = AbstractVector
+
+## power of radix 2 (flooring mode)
+function pwr2(x::Int64)
+    x > 0 || error("pwr2(x): x should be positive!")
+    r = 0
+    x > 4294967295 && (x >>= 32; r += 32)
+    x >      65535 && (x >>= 16; r += 16)
+    x >        255 && (x >>=  8; r +=  8)
+    x >         15 && (x >>=  4; r +=  4)
+    x >          3 && (x >>=  2; r +=  2)
+    x >          1 && (x >>=  1; r +=  1)
+
+    return r
+end
+
+function twiddle!(wa::VecI{Complex{T}}) where T<:AbstractFloat
     Nby2 = length(wa)
     sinθ, cosθ = sincospi(inv(Nby2)) # sin(θ), cos(θ)
 
@@ -126,6 +142,69 @@ function ditnn!(sa::VecI{Complex{T}}, ba::VecI{Complex{T}},
     end
 
     return nothing
+end
+
+"""
+Radix2 Kernel for 1D FFT
+
+Initialize and return a kernel with `fftsize` and specific type `T<:AbstractFloat`.
+
+    FFTKernel{T}(fftsize::Integer) where T<:AbstractFloat
+
+Initialize and return a kernel with `fftsize` and default type `Float64`.
+
+    FFTKernel(fftsize::Integer)
+"""
+struct FFTKernel{T<:AbstractFloat}
+    cache  ::Vector{Complex{T}}
+    twiddle::Vector{Complex{T}}
+    fftsize::Int
+    ifswap ::Bool
+
+    function FFTKernel{T}(fftsize::Integer) where T<:AbstractFloat # type-stability ✓
+        # fftsize should be power of 2
+        cache   = Vector{Complex{T}}(undef, fftsize)
+        twiddle = Vector{Complex{T}}(undef, fftsize >> 1)
+
+        return new{T}(cache, twiddle!(twiddle), fftsize, isone(pwr2(fftsize) & 1))
+    end
+
+    FFTKernel(fftsize::Integer) = FFTKernel{Float64}(fftsize)
+end
+
+"""
+    fft!(x::AbstractVector{Complex{T}}, f::FFTKernel{T}) where T<:AbstractFloat
+
+Perform Fast-Fourier Transform (FFT) in place on the signal sequence `x` using an FFT kernel `f`. The input sequence `x` will be overwritten by the FFT result.
+"""
+function fft!(x::VecI{Complex{T}}, f::FFTKernel{T}) where T<:AbstractFloat
+    cache = f.cache
+    if f.ifswap
+        @simd for i in eachindex(cache)
+            @inbounds cache[i] = x[i]
+        end
+        ditnn!(cache, x, f.twiddle, f.fftsize >> 1)
+    else
+        ditnn!(x, cache, f.twiddle, f.fftsize >> 1)
+    end
+
+    return x
+end
+
+"""
+    fft(x::AbstractVector{Complex{T}}, f::FFTKernel{T}) where T<:AbstractFloat
+    fft(x::AbstractVector{T},          f::FFTKernel{T}) where T<:AbstractFloat
+
+Similar to `fft!`, the function will pass the **copy** of the given signal sequence to the `fft!` routine.
+"""
+fft(x::VecI{Complex{T}}, f::FFTKernel{T}) where T<:AbstractFloat = fft!(copy(x), f)
+
+function fft(x::VecI{T}, f::FFTKernel{T}) where T<:AbstractFloat
+    cx = similar(x, Complex{T})
+    @simd for i in eachindex(cx)
+        @inbounds cx[i] = x[i]
+    end
+    return fft!(cx, f)
 end
 
 end
